4c402932774cdb928c150fed672df4c2d3154b24
marduk191 <marduk191@gmail.com>
2026-03-28 22:52:41 -0500

    rolling back

--- __init__.py
+++ __init__.py
@@ -1,22 +1,13 @@
-from .nodes import (
-    VoxtralTTSFixed,
-    VoxtralTTSClone,
-    VoxtralExtractVoice,
-    VoxtralTTSLocal,
-)
+from .nodes import VoxtralTTSFixed, VoxtralTTSClone
 
 NODE_CLASS_MAPPINGS = {
-    "VoxtralTTSFixed":     VoxtralTTSFixed,
-    "VoxtralTTSClone":     VoxtralTTSClone,
-    "VoxtralExtractVoice": VoxtralExtractVoice,
-    "VoxtralTTSLocal":     VoxtralTTSLocal,
+    "VoxtralTTSFixed": VoxtralTTSFixed,
+    "VoxtralTTSClone": VoxtralTTSClone,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "VoxtralTTSFixed":     "Voxtral TTS Fixed Voice",
-    "VoxtralTTSClone":     "Voxtral TTS Voice Clone",
-    "VoxtralExtractVoice": "Voxtral Extract Voice Embedding",
-    "VoxtralTTSLocal":     "Voxtral TTS Local",
+    "VoxtralTTSFixed": "Voxtral TTS Fixed Voice",
+    "VoxtralTTSClone": "Voxtral TTS Voice Clone",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
